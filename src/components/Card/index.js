81dba2ef82c5d17f2eb485befc53aedca8e15e72
Guibson Meirelles Moura <mouraguibson12@gmail.com>
2022-01-07 08:33:31 -0400

    arquivos adiconais

---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+export default function(){
+    <Card>
+        <CardContent>
+
+        </CardContent>
+        
+    </Card>
+}
---
+++ src/screens/lanches/styles.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const estiloContainer = {
+    
+    p: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+}
+
+export const estiloAppBar = {
+    flexDirection:'row',
+    justifyContent:'center',
+    mb:3
+}
+
+export const estiloCaxiaTexto = {
+    display:'flex',
+    justifyContent:'center',
+    flexGrow: 2,
+    mr:3
+}
+
+export const estiloGridItem = {
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+}
